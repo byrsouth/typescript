0bf33c2107993df2fbee26c9bde8d8f9e67ed637
byrsouth <byron.southerland@icloud.com>
2019-12-03 20:46:17 -0500

    Array Types

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,6 +1,13 @@
 var person = {
     name: 'Byron',
     age: 48,
-    nickname: 'Five0'
+    nickname: 'Five0',
+    hobbies: ['Sports', 'Chess']
 };
+var favoriteActivities;
+favoriteActivities = ['Chess'];
 console.log(person.nickname);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,7 +1,15 @@
 const person = {
   name: 'Byron',
   age: 48,
-  nickname: 'Five0'
+  nickname: 'Five0',
+  hobbies: ['Sports', 'Chess']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Chess'];
+
 console.log(person.nickname);
+
+for (const hobby of person.hobbies ) {
+    console.log(hobby);
+}
