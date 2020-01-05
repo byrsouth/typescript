53f2c87b0c2706f2ca0c3d743f46ce78a440096f
byrsouth <byron.southerland@icloud.com>
2020-01-05 17:57:42 -0500

    literal-types

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,17 +1,23 @@
-function combine(input1: number | string, input2: number | string) {
-    let result;
-    if( typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
-    } else if( typeof input1 === 'string' && typeof input2 === 'string'){
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text') {
+
+    let result: any;
+    if( typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2
+    } else {
         result = input1.toString() + input2.toString();
     }
-    
+
     return result; 
   }
 
-  const combinedAges = combine(30 ,26);
-
+  const combinedAges = combine(30 ,26, 'as-number');
   console.log(combinedAges);
 
-  const combinedNames = combine('Max', 'Anna');
-  console.log(combinedNames);
+  const combinedStringAges = combine('30', '26', 'as-number');
+  console.log(combinedStringAges);
+
+  const combinedNames = combine(2, 'Anna', 'as-text');
+  console.log(combinedNames);  
