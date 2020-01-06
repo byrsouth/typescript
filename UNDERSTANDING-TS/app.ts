b01a58d6d2d7734d7bd69237041ed6688f9eb391
byrsouth <byron.southerland@icloud.com>
2020-01-05 20:56:15 -0500

    void return type

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,26 +1,9 @@
-type Combinable = number | string;
-type ConversonDescriptor = 'as-number' | 'as-text';
+function add(n1: number, n2: number): number{
+    return n1 + n2;
+}
 
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversonDescriptor) {
+function printResults(num: number): void{
+    console.log('Result: ' + num);    
+}
 
-    let result: Combinable;
-    if( typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
-        result = +input1 + +input2
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-
-    return result; 
-  }
-
-  const combinedAges = combine(30 ,26, 'as-number');
-  console.log(combinedAges);
-
-  const combinedStringAges = combine('30', '26', 'as-number');
-  console.log(combinedStringAges);
-
-  const combinedNames = combine(2, 'Anna', 'as-text');
-  console.log(combinedNames);  
+printResults(add(5, 12));
