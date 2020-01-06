714c916946f7560dd14c79a2611e6eb3b6c2ffe1
byrsouth <byron.southerland@icloud.com>
2020-01-05 20:31:21 -0500

    literal types

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,9 +1,12 @@
-function combine(
-    input1: number | string,
-    input2: number | string,
-    resultConversion: 'as-number' | 'as-text') {
+type Combinable = number | string;
+type ConversonDescriptor = 'as-number' | 'as-text';
 
-    let result: any;
+function combine(
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion: ConversonDescriptor) {
+
+    let result: Combinable;
     if( typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
         result = +input1 + +input2
     } else {
