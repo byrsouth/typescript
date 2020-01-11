74fea305eaabc7c86efc2ddd24e9e02eee9fa2c7
byrsouth <byron.southerland@icloud.com>
2020-01-11 18:48:25 -0500

    funtions as parameter

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -4,7 +4,21 @@ function add(n1, n2) {
 function printResults(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+function logName(name, cb) {
+    var retVal = name;
+    cb(retVal);
+}
 printResults(add(5, 12));
 var combinedValues;
 combinedValues = add;
 console.log(combinedValues(4, 9));
+addAndHandle(10, 20, function (sum) {
+    console.log(sum);
+});
+logName('Byron', function (value) {
+    console.log(value);
+});
